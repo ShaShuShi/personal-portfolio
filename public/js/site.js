618214a3e8f43e9eba4fd8e44c6b2147d0cd762b
ShaShuShi <58037426+ShaShuShi@users.noreply.github.com>
2024-12-13 01:18:54 -0600

    Initial commit

---
+++ public/js/site.js
@@ -0,0 +1,14 @@
+﻿function expandProject(card) {
+    const expandedClass = 'expanded';
+    const alreadyExpanded = card.classList.contains(expandedClass);
+
+    // Close all expanded cards before expanding another
+    document.querySelectorAll('.project-card.expanded').forEach(expandedCard => {
+        expandedCard.classList.remove(expandedClass);
+    });
+
+    // Toggle expansion
+    if (!alreadyExpanded) {
+        card.classList.add(expandedClass);
+    }
+}
